38e2474ca3ecd1c754abe3b0ba1b760fadf3526a
griffinwalkerMM <griffin@metismachine.com>
2018-11-12 14:05:12 -0500

    Initial push to skafos

--- .ipynb_checkpoints/activity_classifier_main-checkpoint.py
+++ .ipynb_checkpoints/activity_classifier_main-checkpoint.py
@@ -5,8 +5,6 @@ import turicreate as tc
 from s3fs.core import S3FileSystem
 import pandas as pd
 
-tc.config.set_num_gpus(0)
-
 
 # Read in input data and convert to SFrame for modeling
 # https://apple.github.io/turicreate/docs/userguide/activity_classifier/data-preparation.html
@@ -15,14 +13,14 @@ tc.config.set_num_gpus(0)
 data_dir = "https://s3.amazonaws.com/skafos.example.data/HaptDataSet/RawData/"
 labels = tc.SFrame.read_csv(data_dir + 'labels.txt', delimiter=' ', header=False, verbose=False)
 labels = labels.rename({'X1': 'exp_id', 'X2': 'user_id', 'X3': 'activity_id', 'X4': 'start', 'X5': 'end'})
-print("Reading in the example data from Skafos Public S3 bucket ...")
+
 
 # Import accelerometer and gyroscope data for each experiment. 
 # Note that we've had to modify the example data prep code since SFrame.read_csv does not work with s3fs
 
 # Get list of files
 s3 = S3FileSystem(anon=True)
-directory_path = "s3://skafos.example.data/HaptDataSet/RawData/";
+directory_path = f's3://skafos.example.data/HaptDataSet/RawData/'
 acc_files = s3.glob(directory_path + 'acc_*.txt')
 gyro_files = s3.glob(directory_path + 'gyro_*.txt')
 
@@ -36,27 +34,23 @@ def find_label_for_containing_interval(intervals, index):
 
 data = tc.SFrame()
 files = zip(sorted(acc_files), sorted(gyro_files))
-counter = 0
-print("Reading in more files....")
 for acc_file, gyro_file in files:
-    counter +=1
-    print(counter)
     exp_id = int(acc_file.split('_')[1][-2:])
     
-    acc_full_path = "s3://" + acc_file
-    gyro_full_path = "s3://" + gyro_file
+    acc_full_path = f's3://' + acc_file
+    gyro_full_path = f's3://' + gyro_file
     
     # Read files as pandas data frame and then convert to SFrame, since SFrame does not currenly play nicely with s3fs. 
     # Accel data first then gyro
     
     # Load accel data
-    acc_pd = pd.read_csv(s3.open(acc_full_path, mode='rb'), sep=' ', header=None, index_col=False)
+    acc_pd = pd.read_csv(s3.open(f'{acc_full_path}', mode='rb'), sep=' ', header=None, index_col=False)
     sf = tc.SFrame(data=acc_pd)
     sf = sf.rename({'0': 'acc_x', '1': 'acc_y', '2': 'acc_z'})
     sf['exp_id'] = exp_id
 
     # Load gyro data
-    gyro_pd = pd.read_csv(s3.open(gyro_full_path, mode='rb'), sep=' ', header=None, index_col=False)
+    gyro_pd = pd.read_csv(s3.open(f'{gyro_full_path}', mode='rb'), sep=' ', header=None, index_col=False)
     gyro_sf = tc.SFrame(data=gyro_pd)
     gyro_sf = gyro_sf.rename({'0': 'gyro_x', '1': 'gyro_y', '2': 'gyro_z'})
     sf = sf.add_columns(gyro_sf)
@@ -83,7 +77,6 @@ target_map = {
     6.: 'laying'
 }
 
-print("filtering the data")
 # Use the same labels used in the experiment
 data = data.filter_by(list(target_map.keys()), 'activity_id')
 data['activity'] = data['activity_id'].apply(lambda x: target_map[x])
@@ -93,14 +86,11 @@ data = data.remove_column('activity_id')
 # Data prep is now complete, and modeling code starts below. 
 
 #Train/test split for modeling
-print("split the data for modeling")
 train, test = tc.activity_classifier.util.random_split_by_session(data, session_id='exp_id', fraction=0.8)
 
 # Create an activity classifier
-print("Creating the activity classifier.....")
 model = tc.activity_classifier.create(train, session_id='exp_id', target='activity', prediction_window=50)
 
-print("Evaluate the model....")
 # Evaluate the model and save the results into a dictionary
 metrics = model.evaluate(test)
 print(metrics['accuracy'])
