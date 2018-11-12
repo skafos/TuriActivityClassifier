76c72d17312fc585d6270a2665f206d9118f1de3
griffinwalkerMM <griffin@metismachine.com>
2018-11-12 15:39:01 -0500

    Rename files, move load_data module, add Skafos back to project

---
+++ activity_classifier.py
@@ -0,0 +1,30 @@
+# Activity classifier prototype with Turi Create
+# Based on example from https://apple.github.io/turicreate/docs/userguide/activity_classifier/
+import turicreate as tc
+from skafossdk import *
+from common.load_data import ActivityData
+import sys
+
+ska = Skafos() # initialize Skafos
+activity_data = ActivityData() # load data loading class
+
+# don't use GPU
+tc.config.set_num_gpus(0)
+
+# get the data
+ska.log("Grabbing the data from the public S3 bucket", labels = ['activity_classifier'])
+data = activity_data.get_data()
+
+#Train/test split for modeling
+ska.log("Splitting the data for modeling", labels = ["activity_classifier"])
+train, test = tc.activity_classifier.util.random_split_by_session(data, session_id='exp_id', fraction=0.8)
+
+# Create an activity classifier
+ska.log("Creating the activity classifier", labels = ["activity_classifier"])
+model = tc.activity_classifier.create(train, session_id='exp_id', target='activity', prediction_window=50)
+
+ska.log("Evaluating the activity classifier model", labels = ['acitivity_classifier'])
+# Evaluate the model and save the results into a dictionary
+metrics = model.evaluate(test)
+
+ska.log(f"Accuracy metrics from the model{metrics['accuracy']}", labels = ['activity_classifier'])
--- common/load_data.py
+++ common/load_data.py
@@ -1,9 +1,10 @@
 import turicreate as tc
 from s3fs.core import S3FileSystem
 import pandas as pd
-from skafossdk import *
+#from skafossdk import Skafos
 
-ska = Skafos()
+#ska = Skafos()
+
 
 def find_label_for_containing_interval(intervals, index):
     containing_interval = intervals[:, 0][(intervals[:, 1] <= index) & (index <= intervals[:, 2])]
@@ -74,7 +75,7 @@ class ActivityData:
             sf['activity_id'] = sf['id'].apply(lambda x: find_label_for_containing_interval(exp_labels, x))
             sf = sf.remove_columns(['id'])
             files_read += 1
-            ska.report("Files Read", y = files_read, y_label = "Count")
+            #ska.report("Files Read", y = files_read, y_label = "Count")
             data = data.append(sf)
 
         return data
