4250e110e1628c18008caa82f768f4b3cee193aa
griffinwalkerMM <griffin@metismachine.com>
2018-11-19 08:39:18 -0500

    Change file paths, remove writing and image epxloring

--- activity_classifier.py
+++ activity_classifier.py
@@ -1,15 +1,15 @@
 # Activity classifier prototype with Turi Create
 # Based on example from https://apple.github.io/turicreate/docs/userguide/activity_classifier/
 import turicreate as tc
-from skafossdk import *
+#from skafossdk import *
 import sys
 import pandas as pd
 from common.load_data import ActivityData
 
-ska = Skafos() # initialize Skafos
+#ska = Skafos() # initialize Skafos
 
 
-ska.log("Grabbing the data", labels = ['activity_classifier'])
+#ska.log("Grabbing the data", labels = ['activity_classifier'])
 # load data loading class
 activity_data = ActivityData()
 data = activity_data.get_data()
@@ -19,21 +19,24 @@ data = activity_data.get_data()
 tc.config.set_num_gpus(0)
 
 # get the data
-ska.log("Grabbing the data from the public S3 bucket", labels = ['activity_classifier'])
+#ska.log("Grabbing the data from the public S3 bucket", labels = ['activity_classifier'])
 
 #Train/test split for modeling
-ska.log("Splitting the data for modeling", labels = ["activity_classifier"])
+#ska.log("Splitting the data for modeling", labels = ["activity_classifier"])
 train, test = tc.activity_classifier.util.random_split_by_session(data, session_id='Experiment', fraction=0.8)
 
 # Create an activity classifier
-ska.log("Creating the activity classifier", labels = ["activity_classifier"])
+#ska.log("Creating the activity classifier", labels = ["activity_classifier"])
 model = tc.activity_classifier.create(train, session_id='Experiment', target='Activity', prediction_window=50)
 
-ska.log("Evaluating the activity classifier model", labels = ['acitivity_classifier'])
+#ska.log("Evaluating the activity classifier model", labels = ['acitivity_classifier'])
 # Evaluate the model and save the results into a dictionary
-metrics = model.evaluate(test)
+#metrics = model.evaluate(test)
 
-ska.log(f"Accuracy metrics from the model: {metrics['accuracy']}", labels = ['activity_classifier'])
+model.save("./activity_classifier_model.model")
+model.export_coreml("./activity_classifier_model.mlmodel")
+
+#ska.log(f"Accuracy metrics from the model: {metrics['accuracy']}", labels = ['activity_classifier'])
 
 # save the model
-ska.engine.save_model('Activity-Classifier', model, tags = ['latest'])
+#ska.engine.save_model('Activity-Classifier', model, tags = ['latest'])
