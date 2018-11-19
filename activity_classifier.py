074014b2332bc9a21d9fa923252aacf83bdd0ba1
griffinwalkerMM <griffin@metismachine.com>
2018-11-19 12:11:03 -0500

    Update saving of the model

--- activity_classifier.py
+++ activity_classifier.py
@@ -1,15 +1,17 @@
 # Activity classifier prototype with Turi Create
 # Based on example from https://apple.github.io/turicreate/docs/userguide/activity_classifier/
 import turicreate as tc
-#from skafossdk import *
+from skafossdk import *
 import sys
 import pandas as pd
 from common.load_data import ActivityData
+from common.save_models import *
 
-#ska = Skafos() # initialize Skafos
+
+ska = Skafos() # initialize Skafos
 
 
-#ska.log("Grabbing the data", labels = ['activity_classifier'])
+ska.log("Grabbing the data", labels = ['activity_classifier'])
 # load data loading class
 activity_data = ActivityData()
 data = activity_data.get_data()
@@ -19,24 +21,32 @@ data = activity_data.get_data()
 tc.config.set_num_gpus(0)
 
 # get the data
-#ska.log("Grabbing the data from the public S3 bucket", labels = ['activity_classifier'])
+ska.log("Grabbing the data from the public S3 bucket", labels = ['activity_classifier'])
 
 #Train/test split for modeling
-#ska.log("Splitting the data for modeling", labels = ["activity_classifier"])
+ska.log("Splitting the data for modeling", labels = ["activity_classifier"])
 train, test = tc.activity_classifier.util.random_split_by_session(data, session_id='Experiment', fraction=0.8)
 
 # Create an activity classifier
-#ska.log("Creating the activity classifier", labels = ["activity_classifier"])
+ska.log("Creating the activity classifier", labels = ["activity_classifier"])
 model = tc.activity_classifier.create(train, session_id='Experiment', target='Activity', prediction_window=50)
 
-#ska.log("Evaluating the activity classifier model", labels = ['acitivity_classifier'])
+
 # Evaluate the model and save the results into a dictionary
-#metrics = model.evaluate(test)
+ska.log("Evaluating the activity classifier model", labels = ['acitivity_classifier'])
+metrics = model.evaluate(test)
 
-model.save("./activity_classifier_model.model")
-model.export_coreml("./activity_classifier_model.mlmodel")
 
-#ska.log(f"Accuracy metrics from the model: {metrics['accuracy']}", labels = ['activity_classifier'])
+ska.log("Saving the model", labels = ['activity_classifier'])
+# export to coreml
+coreml_model_name = "activity_classifier.mlmodel"
+res = model.export_coreml(coreml_model_name)
 
-# save the model
-#ska.engine.save_model('Activity-Classifier', model, tags = ['latest'])
+# compress the model
+compressed_model_name, compressed_model = compress_model(coreml_model_name)
+
+# save to Skafos
+sm.skafos_save_model(skafos = ska, model_name = compressed_model_name,
+								compressed_model = compressed_model,
+								permissions = 'public')
+
---
+++ common/save_models.py
@@ -0,0 +1,65 @@
+import gzip
+import os
+
+
+'''
+from common.save_model import *
+
+When you are ready to save your model, your workflow 
+should look like the following:
+
+# export to coreml
+coreml_model_name = "recommender.mlmodel"
+res = model.export_coreml(coreml_model_name)
+
+# compress the model
+compressed_model_name, compressed_model = compress_model(coreml_model_name)
+
+# save to Skafos
+skafos_save_model(skafos = ska, model_name = compressed_model_name,
+								compressed_model = compressed_model,
+								permissions = 'public')
+
+'''
+
+def load_mlmodel(name):
+	# load the saved coreml_model
+    with open(name, 'rb') as data:
+        result = data.read()
+    return result
+
+def compress_model(coreml_model_name):
+	'''
+	Takes as input the name of the model saved
+	Returns a compressed model to be saved to Skafos
+	'''
+	time.sleep(10) # wait for full-model to be written to file
+
+	# load up the model written to file
+	mc = load_mlmodel(coreml_model_name)
+
+	# re-name the model so user knows its compressed on save
+	compressed_model_name = coreml_model_name + ".gzip"
+	# compress the model
+	compressed_model = gzip.compress(mc)
+
+	return compressed_model_name, compressed_model
+
+
+
+def skafos_save_model(skafos, model_name, compressed_model, tags = ['latest'], permissions = "private"):
+	# try saving the model
+	try:
+		res = skafos.engine.save_model(coreml_model_name, compressed_model, tags, permissions)
+		print(res.result(), flush=True)
+	except Exception as err:
+		# if we get an error, try again
+		print(f"Unable to save the model = {err}, trying again ...")
+		try:
+			res = skafos.engine.save_model(coreml_model_name, compressed_model, tags, permissions)
+			print(res.result(), flush=True)
+		# if after 2 tries it still won't save, give up and return the error
+		except Exception as err2:
+			print(f"Unable to save the model = {err2}, exiting")
+
+
