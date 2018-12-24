71d0a5aa36d2afad38bea40ebb8efc492cc2570b
griffinwalkerMM <griffin@metismachine.com>
2018-12-24 10:04:58 -0500

    Update turicreate install

--- common/load_turicreate.py
+++ common/load_turicreate.py
@@ -1,4 +1,33 @@
-def install_turicreate():
+from subprocess import check_output
+import sys
+import os
+
+
+def try_install(timeout):
+    print(f"\n Trying to install TuriCreate with timeout: {timeout} seconds")
+    try:
+        # try installing, give it 3 mins to try and install 
+        output = check_output(["pip", "install", "turicreate==5.1"], timeout=timeout)
+    except:
+        print("Timeout install for turicreate")
+        
+    try:
+        # load turicreate directly from site-packages (avoid restart kernel)
+        if not os.path.exists(".local/site-packages/"):
+            os.makedirs(".local/site-packages/")
+        sys.path.append(".local/site-packages/")
+        import turicreate as tc
+        tc.SFrame() # check to see that we can load an empty SFrame
+        print("Turi Create is now installed. Reference tc from here on out.")
+        return tc
+    except Exception as e:
+        raise e
+        return False
+        
+
+
+
+def install_turicreate(timeout=300, retries = 2):
     # check to see if it is already loaded and good to go
     try:
         import turicreate as tc
@@ -8,21 +37,11 @@ def install_turicreate():
     # if that fails, install it
     except:
         print("Turi Create was not installed, installing now.")
-        from subprocess import check_output
-        import sys
-        try:
-            # try installing, give it 3 mins to try and install 
-            output = check_output(["pip", "install", "turicreate==5.1"], timeout=180)
-        except:
-            print("Timeout install for turicreate")
+        for r in range(retries):
+            try:
+                tc = try_install(timeout)
+                return tc
+            except:
+                timeout = timeout*2
         
-        try:
-            # load turicreate directly from site-packages (avoid restart kernel)
-            sys.path.append("./local/site-packages/")
-            import turicreate as tc
-            tc.SFrame() # check to see that we can load an empty SFrame
-            print("Turi Create is now installed. Reference tc from here on out.")
-            return tc
-        except:
-            print("Unable to install and load turicreate, please go to terminal and run pip install turicreate")
-
+        print("\n Was unable to install TuriCreate")
