0fe3e5456ac017ddded81933f6d12d48389d2221
tylerhutcherson <tyler.hutcherson@metismachine.com>
2019-03-26 13:49:01 -0400

    add new dependency module removing friction from user flow

--- utilities/dependencies.py
+++ utilities/dependencies.py
@@ -4,23 +4,24 @@ import sys
 from subprocess import check_output
 
 
-def _try_install(timeout):
+def _requirements(timeout):
     try:
-        # try installing, give it 3 mins to try and install 
+        # try installing, give it 3 mins to try and install
         output = check_output(["pip", "install", "-r", "/home/jovyan/requirements.txt"], timeout=timeout)
-        
     except Exception as e:
         print(f"Exception encountered while installing dependencies: {e}", flush=True)
         raise e
 
 def install(timeout=300, retries=2):
     # Install all dependencies inside requirements.txt
-    print("Checking and installing required python dependencies in requirements.txt", flush=True)
-    try:
-        _try_install(timeout)
-        # Load pkg directly from site-packages (avoid restart kernel)  
-        if not os.path.exists(".local/site-packages/"):
-            os.makedirs(".local/site-packages/")
-        sys.path.append(".local/site-packages/")
-    except Exception as e:
-        raise e
+    if os.path.exists("/home/jovyan/"):
+        print("Checking and installing required python dependencies in requirements.txt", flush=True)
+        try:
+            _requirements(timeout)
+            # Load pkg directly from site-packages (avoid restart kernel)
+            if not os.path.exists(".local/site-packages/"):
+                os.makedirs(".local/site-packages/")
+            sys.path.append(".local/site-packages/")
+        except Exception as e:
+            raise e
+        print("Done", flush=True)
